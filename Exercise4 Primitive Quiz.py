fbe5e0b1454033ce1cad6099247df7718210b1a3
jisajohnson <jisajohnsonjj@gmail.com>
2024-11-05 21:19:06 +0400

    Exercise 4 addition
    Created a quiz which is case insensitive so as long as the answer is correct. It will show the correct answer otherwise it will be wrong

---
+++ Exercise4 Primitive Quiz.py
@@ -0,0 +1,100 @@
+#4. Primitive Quiz
+question=input("What is the capital of France?")
+answer="Paris"
+
+if question.lower()==answer.lower():
+    print("Correct! Congratulations!",answer,"is the right answer")
+else:
+    print("Wrong! The answer is",answer)
+
+#Advanced Requirements
+print("Capitals of European Countries Quiz!")
+print("Question 1")
+question1=input("What is the Capital of Germany?")
+answer1="Berlin"
+
+if question1.lower()==answer1.lower():
+    print("Correct!",answer1,"is the right answer!")
+else:
+    print("Wrong answer! The correct answer is",answer1)
+
+print("Question 2")
+question2=input("What is the capital of Spain?")
+answer2="Madrid"
+
+if question2.lower()==answer2.lower():
+    print("Correct!",answer2,"is the right answer!")
+else:
+    print("Wrong answer! The correct answer is",answer2)
+
+print("Question 3")
+question3=input("What is the capital of Norway?")
+answer3="Oslo"
+
+if question3.lower()==answer3.lower():
+    print("Correct!",answer3,"is the right answer!")
+else:
+    print("Wrong answer! The correct answer is",answer3)
+
+print("Question 4")
+question4=input("What is the capital of Finland?")
+answer4="Helsinki"
+
+if question4.lower()==answer4.lower():
+    print("Correct!",answer4,"is the right answer!")
+else:
+    print("Wrong answer! The correct answer is",answer4)
+
+print("Question 5")
+question5=input("What is the capital of Denmark?")
+answer5="Coppenhagen"
+
+if question5.lower()==answer5.lower():
+    print("Correct!",answer5,"is the right answer!")
+else:
+    print("Wrong answer! The correct answer is",answer5)
+
+print("Question 6")
+question6=input("What is the capital of Portugal?")
+answer6="Lisbon"
+
+if question6.lower()==answer6.lower():
+    print("Correct!",answer6,"is the right answer!")
+else:
+    print("Wrong answer! The correct answer is",answer6)
+
+print("Question 7")
+question7=input("What is the capital of Poland?")
+answer7="Warsaw"
+
+if question7.lower()==answer7.lower():
+    print("Correct!",answer7,"is the right answer!")
+else:
+    print("Wrong answer! The correct answer is",answer7)
+
+print("Question 8")
+question8=input("What is the capital of Sweden?")
+answer8="Stockholm"
+
+if question8.lower()==answer8.lower():
+    print("Correct!",answer8,"is the right answer!")
+else:
+    print("Wrong answer! The correct answer is",answer8)
+
+print("Question 9")
+question9=input("What is the capital of Slovakia?")
+answer9="Bratislava"
+
+if question9.lower()==answer9.lower():
+    print("Correct!",answer9,"is the right answer!")
+else:
+    print("Wrong answer! The correct answer is",answer9)
+
+print("Question 10")
+question10=input("What is the capital of Ukraine?")
+answer10="Kyiv"
+
+if question10.lower()==answer10.lower():
+    print("Correct!",answer10,"is the right answer!")
+else:
+    print("Wrong answer! The correct answer is",answer10)
