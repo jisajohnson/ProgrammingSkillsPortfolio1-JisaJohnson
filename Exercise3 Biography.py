3be45cabfc65aa785194dbfa68cd9f13a509cdb2
jisajohnson <jisajohnsonjj@gmail.com>
2024-11-06 20:56:23 +0400

    Update
    Removed the def function, made the code more simpler and used  a different separation function using key-value

--- Exercise3 Biography.py
+++ Exercise3 Biography.py
@@ -2,12 +2,16 @@
 information={"Name":"Jisa Johnson",
              "Hometown":"Fujairah",
              "Age":20} #creating a dictionary with personal details
-print(information,sep='\n') #Prints the information in separate lines
+for key, value in information.items(): #Using for function with key-values to show in separate line
+    print(f"{key}:{value}") #Prints the information in separate lines
 
-#Advanced Requirements 
-def personal_details(): #define a function called personal details
-    name=input("What is your name?") #Asks the user to type their name
-    hometown=input("Where is your hometown?") #Asks the user their hometown
-    age=input(str("what is your age?")) #Asks the user their age and in case it is written in string it accepts that as well
-    print("Name:{}\nHometown:{}\nAge:{}".format(name,hometown,age)) #Prints the personal details separately as per given by the user
-personal_details() #End of the define function
+#Advanced Requirements
+name=input("What is your name?") #Asks the user to type their name
+hometown=input("Where is your hometown?") #Asks the user their hometown
+age=input(str("what is your age?")) #Asks the user their age and in case it is written in string it accepts that as well
+
+personal_details={"Name":name,
+                  "Hometown":hometown,
+                  "age":age} #The overall dictionary of the user's input data
+for key, value in personal_details.items(): #to ensure it comes in separate lines
+    print(f"{key}:{value}") #prints the personal_details in separate lines
