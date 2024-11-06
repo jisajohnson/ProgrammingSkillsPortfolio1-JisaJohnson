b611639f51f2b8fd9701e8ac1a76052c4c81e4c8
jisajohnson <jisajohnsonjj@gmail.com>
2024-11-06 16:44:24 +0400

    Corrections
    1. Gave spaces between Coding is Cool!
    2. Made sure the information is printed as a dictionary and not a tuple.

--- Exercise3 Biography.py
+++ Exercise3 Biography.py
@@ -1,12 +1,13 @@
 #3. Biography
-information = {"Name":"Jisa Johnson",
-               "Hometown":"Fujairah",
-               "Age":21}
-print(*information.items(),sep='\n')
+information={"Name":"Jisa Johnson",
+             "Hometown":"Fujairah",
+             "Age":20} #creating a dictionary with personal details
+print(information,sep='\n') #Prints the information in separate lines
 
-def personal_details():
-    name=input("What is your name?")
-    hometown=input("Where is your hometown?")
-    age=input(str("what is your age?"))
-    print("Name:{}\nHometown:{}\nAge:{}".format(name,hometown,age))
-personal_details()
+#Advanced Requirements 
+def personal_details(): #define a function called personal details
+    name=input("What is your name?") #Asks the user to type their name
+    hometown=input("Where is your hometown?") #Asks the user their hometown
+    age=input(str("what is your age?")) #Asks the user their age and in case it is written in string it accepts that as well
+    print("Name:{}\nHometown:{}\nAge:{}".format(name,hometown,age)) #Prints the personal details separately as per given by the user
+personal_details() #End of the define function
