6dfb351c619f6f92cbb3707b91f2289517394509
jisajohnson <jisajohnsonjj@gmail.com>
2024-11-12 21:23:21 +0400

    Exercise 7 complete
    for and while loops

---
+++ Exercise7 Some Counting.py
@@ -0,0 +1,29 @@
+#7. Some Counting
+#0 to 50 increments of 1
+print("A loop that counts up from 0 to 50 in increments of 1")
+for i in range(1,51): #a loop that counts from 0 to 50 in increment of 1
+    print(i)
+
+#Counts down from 50 to 0 in decrement of 1.
+print("A loop that counts down from 50 to 0 in decrements of 1")
+num=50 #Declaring a number for num
+while num >= 0: #using while loop 
+    print(num) #
+    num -=1  #counts down and shows in the decrements of 1
+
+#Counts up from 30 to 50 in decrements of 1
+print("A loop that counts up from 30 to 50 in increments of 1")
+for i in range(30,51):
+    print(i)
+
+#Counts down from 50 to 10 in decrements of 2
+print("A loop that counts down from 50 to 10 in decrements of 2")
+num=50 
+while num >=10:
+    print(num)
+    num -=2
+
+#counts up from 100 to 200 in increments of 5
+print("A loop that counts up from 100 to 200 in increments of 5")
+for i in range(100,201,5):
+    print(i)
