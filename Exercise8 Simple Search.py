151f9c3d624b47216933a14a3a6da052d28f1a97
jisajohnson <jisajohnsonjj@gmail.com>
2024-11-12 21:55:32 +0400

    Ex8 Completed
    Might have to do corrections in future

---
+++ Exercise8 Simple Search.py
@@ -0,0 +1,8 @@
+#8.Simple Search
+Name=["Jake","Zac","Ian","Ron","Sam","Dave"]
+check=input("Enter a name:")
+
+if check in Name:
+    print(check,"is there in the list")
+else:
+    print(check,"is not there in the list")
