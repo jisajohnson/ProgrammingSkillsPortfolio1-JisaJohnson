4a79044fa0cab364833836b8c1e2b38e89300879
jisajohnson <jisajohnsonjj@gmail.com>
2024-11-14 18:51:27 +0400

    added comments
    to explain what each function does, I have added comments

--- Exercise6 Brute Force Attack.py
+++ Exercise6 Brute Force Attack.py
@@ -1,17 +1,17 @@
 #6. Brute Force Attack
-correctpassword=12345
-attempts=0
+correctpassword=12345 #The correct password set by us
+attempts=0 #the number of attempts so far
 
-while attempts<5:
-    password=int(input("Enter the password:"))
+while attempts<5: #when the attempts are less than 5 and the attempts is 0
+    password=int(input("Enter the password:")) #this statement will be printed
 
-    if password == correctpassword:
-        print("Access Granted. You have logged in successfully")
-        break
-    else:
-        attempts += 1
-        attemptsleft = 5-attempts
-        print("Access Denied, Try again. You have", attemptsleft, "attempts remaining!")
+    if password == correctpassword: #if the password is same as the correct password
+        print("Access Granted. You have logged in successfully") #this statement will be printed
+        break #to exit the loop since the condition is met
+    else: #when the condition is not met, it will repeat again
+        attempts += 1 #the number of attempts will be increased
+        attemptsleft = 5-attempts #this shows the number of attempts left
+        print("Access Denied, Try again. You have", attemptsleft, "attempts remaining!") #when the password is wrong this statement will be printed
 
-if attempts == 5:
-    ("Access Denied! The maximum number of attempts is reached. The Authorities have been alerted ")
+if attempts == 5: #When the attempts is more than 5, the loop will be stopped
+    ("Access Denied! The maximum number of attempts is reached. The Authorities have been alerted ") #this would be final statement since they couldn't crack the password
--- Exercise8 Simple Search.py
+++ Exercise8 Simple Search.py
@@ -1,8 +1,8 @@
 #8.Simple Search
-Names=["Jake","Zac","Ian","Ron","Sam","Dave"]
-check=input("Enter a name:").capitalize()
+Names=["Jake","Zac","Ian","Ron","Sam","Dave"] #Create a list with the listed names
+check=input("Enter a name:").capitalize() #Ask the user to input the name they are searching for and it capitalizses the name
 
-if check.lower() in (name.lower() for name in Names):
-    print(check,"is there in the list")
-else:
-    print(check,"is not there in the list")
+if check.lower() in (name.lower() for name in Names): #To check if the person is there in the list and made it case insensitive
+    print(check,"is there in the list") #If the person is there in the list, then you will see this in the terminal
+else: 
+    print(check,"is not there in the list") #if the person is not there in the list, this statement will be printed
