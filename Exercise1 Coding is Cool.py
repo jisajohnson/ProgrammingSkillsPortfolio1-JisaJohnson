d82b3348c37d9d70c94ea3d165ccd04a058fac7b
jisajohnson <jisajohnsonjj@gmail.com>
2024-11-21 11:12:11 +0400

    added comments
    comments based of my understanding on the code

--- Exercise1 Coding is Cool.py
+++ Exercise1 Coding is Cool.py
@@ -1,7 +1,7 @@
 #1. Coding is Cool
-word1 = "Coding " 
-word2 = "is "
-word3 = "Cool!"
+word1 = "Coding " #Declaring Word1 with string value
+word2 = "is " #Declaring Word2 with string value
+word3 = "Cool!" #Declaring Word3 with string value
 
 # Use string concatenation to combine the variables and print the phrase
-print(word1+word2+word3)
+print(word1+word2+word3) #prints the string together according to what order is has been placed
