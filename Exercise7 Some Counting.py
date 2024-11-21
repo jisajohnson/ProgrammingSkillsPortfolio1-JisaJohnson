57f1320a33ce3ba12c0830c35ad7bf94afd2ae6f
jisajohnson <jisajohnsonjj@gmail.com>
2024-11-21 11:17:34 +0400

    added comments
    To make the viewer understand more about the what each function does

--- Exercise7 Some Counting.py
+++ Exercise7 Some Counting.py
@@ -8,22 +8,22 @@ for i in range(0,51): #a loop that counts from 0 to 50 in increment of 1
 print("A loop that counts down from 50 to 0 in decrements of 1")
 num=50 #Declaring a number for num
 while num >= 0: #using while loop 
-    print(num) #
+    print(num) 
     num -=1  #counts down and shows in the decrements of 1
 
 #Counts up from 30 to 50 in decrements of 1
 print("A loop that counts up from 30 to 50 in increments of 1")
-for i in range(30,51):
+for i in range(30,51): #loop that counts from 30 to 50
     print(i)
 
 #Counts down from 50 to 10 in decrements of 2
 print("A loop that counts down from 50 to 10 in decrements of 2")
-num=50 
-while num >=10:
+num=50 #Declaring a variable
+while num >=10: #using while loop to count down from 50 to 10 
     print(num)
-    num -=2
+    num -=2  #counts down in the decremnent of 2
 
 #counts up from 100 to 200 in increments of 5
 print("A loop that counts up from 100 to 200 in increments of 5")
-for i in range(100,201,5):
+for i in range(100,201,5): #counts from 100 to 200 in increments of 5
     print(i)
